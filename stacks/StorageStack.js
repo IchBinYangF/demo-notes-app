00627c1486aa94eb5f1cf06dafdf8db717217111
Yang Fu <yangfu@MacBook-Pro-von-Yang.local>
2023-05-24 08:58:17 +0200

    Adding a storage stack

---
+++ stacks/StorageStack.js
@@ -0,0 +1,21 @@
+import { Bucket, Table } from "sst/constructs";
+
+//DynamoDB and S3
+export function StorageStack({ stack, app }) {
+  // Create the DynamoDB table
+  const table = new Table(stack, "Notes", {
+    fields: {
+      userId: "string",
+      noteId: "string",
+    },
+    primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
+  });
+  // Create an S3 bucket
+  const bucket = new Bucket(stack, "Uploads");
+
+  return {//allow reference this resource in our other stacks.
+    table,
+    bucket,
+  };
+  
+}
