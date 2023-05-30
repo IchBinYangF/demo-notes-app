427804a188342aaf73459181259f035b3b3ba69a
Yang Fu <yangfu@MacBook-Pro-von-Yang.local>
2023-05-30 09:26:48 +0200

    users and authentication

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -7,6 +7,7 @@ export function ApiStack({ stack, app }) {
   // Create the API
   const api = new Api(stack, "Api", {
     defaults: {
+      authorizer: "iam", //tells our API that we want to use AWS_IAM across all our routes
       function: {
         bind: [table],
       },
